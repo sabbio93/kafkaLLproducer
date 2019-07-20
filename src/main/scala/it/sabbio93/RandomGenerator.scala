f8440555e8cbcd64be88528af37b3fb0c1329151
sabbio93 <sabbio.a93@gmail.com>
2019-07-20 11:27:10 +0200

    env var get properly setted

--- src/main/scala/it/sabbio93/RandomGenerator.scala
+++ src/main/scala/it/sabbio93/RandomGenerator.scala
@@ -9,39 +9,33 @@ object RandomGenerator {
 
   def main(args: Array[String]): Unit = {
 
-    val baseLat = 43.836833
-    val baseDouble = 9.463728
-    val deltaLat = 1.275308
-    val deltaDouble = 3.232666
-    val server = Properties.envOrElse("SERVER", "localhost:9092")
-    val topic = Properties.envOrElse("TOPIC", "test")
-    val threads = Integer.parseInt(Properties.envOrElse("THREADS", "10"))
-
-
-    val random = new scala.util.Random()
+    val baseLat   = Properties.envOrNone("BASELAT").map(value=>value.toDouble).getOrElse(43.836833)
+    val baseLong  = Properties.envOrNone("BASELONG").map(value=>value.toDouble).getOrElse(9.463728)
+    val deltaLat  = Properties.envOrNone("DELTALAT").map(value=>value.toDouble).getOrElse(1.275308)
+    val deltaLong = Properties.envOrNone("DELTALONG").map(value=>value.toDouble).getOrElse(3.232666)
+    val server    = Properties.envOrElse("SERVER", "localhost:9092")
+    val topic     = Properties.envOrElse("TOPIC", "test")
+    val threads   = Properties.envOrNone("THREADS").map(value=>value.toInt).getOrElse(1)
 
     val props = new java.util.Properties()
     props.put("bootstrap.servers", server) //cambiare con nome variabile
     props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer")
     props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer")
 
-
     val executor = Executors.newFixedThreadPool(threads)
-    executor.submit(new Producer(deltaLat, deltaDouble, baseLat, baseDouble, props, topic))
-
-    val producer = new KafkaProducer[String, String](props)
+    executor.submit(new Producer(deltaLat, deltaLong, baseLat, baseLong, props, topic))
 
   }//main
 }//RandomGenerator
 
 class Producer(deltaLat: Double, deltaDouble: Double, baseLat: Double, baseDouble: Double, props: java.util.Properties, topic: String) extends Runnable {
 
-  def run: Unit = {
+  def run(): Unit = {
 
     val producer = new KafkaProducer[String, String](props)
-    while (true) //TODO set listener to system signaling
+    val random= new scala.util.Random()
+    while (true)
     {
-      val random = new scala.util.Random()
       val lat = ((random.nextFloat() * 10) % deltaLat) + baseLat
       val long = ((random.nextFloat() * 10) % deltaDouble) + baseDouble
       val value = "lat:" + lat + ",long:" + long
