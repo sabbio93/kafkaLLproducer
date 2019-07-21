044b5ddb8c2fdc2e55c39ec6bc08530667c9624d
sabbio93 <sabbio.a93@gmail.com>
2019-07-21 16:43:24 +0200

    merged bad

--- src/main/scala/it/sabbio93/RandomGenerator.scala
+++ src/main/scala/it/sabbio93/RandomGenerator.scala
@@ -9,6 +9,7 @@ object RandomGenerator {
 
   def main(args: Array[String]): Unit = {
 
+<<<<<<< Updated upstream
     val baseLat   = Properties.envOrNone("BASELAT").map(value=>value.toDouble).getOrElse(43.836833)
     val baseLong  = Properties.envOrNone("BASELONG").map(value=>value.toDouble).getOrElse(9.463728)
     val deltaLat  = Properties.envOrNone("DELTALAT").map(value=>value.toDouble).getOrElse(1.275308)
@@ -19,9 +20,25 @@ object RandomGenerator {
 
     val props = new java.util.Properties()
     props.put("bootstrap.servers", server) //cambiare con nome variabile
+=======
+    val baseLat = 43.836833
+    val baseLong = 9.463728
+    val deltaLat = 1.275308
+    val deltaLong = 3.232666
+    val server= Properties.envOrElse("SERVER","localhost:9092")
+    val topic= Properties.envOrElse("TOPIC","test")
+
+
+    println("loggin on server: "+server)
+    val random = new scala.util.Random()
+
+    val props = new java.util.Properties()
+    props.put("bootstrap.servers", server)
+>>>>>>> Stashed changes
     props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer")
     props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer")
 
+<<<<<<< Updated upstream
     val executor = Executors.newFixedThreadPool(threads)
     executor.submit(new Producer(deltaLat, deltaLong, baseLat, baseLong, props, topic))
 
@@ -35,6 +52,9 @@ class Producer(deltaLat: Double, deltaDouble: Double, baseLat: Double, baseDoubl
     val producer = new KafkaProducer[String, String](props)
     val random= new scala.util.Random()
     while (true)
+=======
+    while (!Thread.currentThread().isInterrupted)//TODO set listener to system signaling
+>>>>>>> Stashed changes
     {
       val lat = ((random.nextFloat() * 10) % deltaLat) + baseLat
       val long = ((random.nextFloat() * 10) % deltaDouble) + baseDouble
